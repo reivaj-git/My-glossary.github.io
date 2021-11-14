c8b44c832b7d52d7b8b712d4bc1b5de8d5098781
ricardo <javi26028@outlook.com>
2021-11-13 23:27:37 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// const navBoton = document.querySelector(".nav-boton");
+// const navegacion = document.querySelector(".navegacion");navBoton.addEventListener("click", () => {
+//    navegacion.classList.toggle("nav-menu_visible");
+
+//    if (navegacion.classList.contains("nav-menu_visible")) {
+//      navBoton.setAttribute("aria-label", "Cerrar menú");
+//    } else {
+//      navBoton.setAttribute("aria-label", "Abrir menú");
+//    }
+//  });
